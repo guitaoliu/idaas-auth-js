a88a818c86aeb0c6c70dfc0fe95e14d7eed52118
Porya Isfahani <porya.isfahani@entrust.com>
2024-07-09 10:39:02 -0400

    add LogoutOptions type to barrel file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { IdaasClient } from "./IdaasClient";
-export type { IdaasClientOptions, UserClaims, LoginOptions, GetAccessTokenOptions } from "./models";
+export type { IdaasClientOptions, UserClaims, LoginOptions, LogoutOptions, GetAccessTokenOptions } from "./models";
