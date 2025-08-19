552edb45038ce7079983bf754c275cf021ee6ff0
Porya Isfahani <porya.isfahani@entrust.com>
2025-08-19 09:10:32 -0400

    fix build errors

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  client: {
-    bundle: true,
-    name: "@hey-api/client-fetch",
-  },
   input: "./authentication.json",
   output: {
     path: "src/models/openapi-ts",
   },
+  plugins: ["@hey-api/client-fetch"],
 });
